b82d34c8886a25b04423fc2356230f0821df0da5
googleoyyp <googleoyyp@163.com>
2018-02-08 13:31:46 +0800

    更新 index.js

--- src/main/webapp/static/js/controllers/main/index.js
+++ src/main/webapp/static/js/controllers/main/index.js
@@ -4,7 +4,7 @@ stareal
     .controller("IndexController", function ($scope, $api, $alert, $document, localStorageService, $state,$timeout,$compile) {
         $scope.mypage = 1;
         $scope.my_sing = 1;//我的页面隐藏gif图
-        $api.get("app/main/latest/local",{},true) //本地最近五条信息
+       $api.get("app/main/latest/local",{},true) //本地最近五条信息
             .then(function (ret) {
                 $scope.datasetData = ret.data.slice(0,5);
                 console.log($scope.datasetData)
@@ -46,7 +46,9 @@ stareal
         $api.get("app/main/ad/retrieve",{})
             .then(function (ret) {
                 $scope.advs = ret.data; //首页轮播
-                angular.element('#carousel-demo').height($document.width() * 29/ 50)
+                console.log($scope.advs);
+                //修改比例29/50
+                angular.element('#carousel-demo').height($document.width() * 0.95);
             })
         //列表
         $api.get("app/main/latest/good",{})
@@ -54,33 +56,54 @@ stareal
                 $scope.latest = ret.data  //列表
             })
         //导航分类
-        $timeout(function () {
-            var swiper = new Swiper('.nav', {
-                slidesPerView: 4,
-                slidesPerGroup : 4,
-                spaceBetween:0,
-                pagination: '.swiper-pagination',//分页容器
-                observer:true//修改swiper自己或子元素时，自动初始化swiper
-            })
-        },0)
+        // $timeout(function () {
+        //     var swiper = new Swiper('.nav', {
+        //         slidesPerView: 4,
+        //         slidesPerGroup : 4,
+        //         spaceBetween:0,
+        //         pagination: '.swiper-pagination',//分页容器
+        //         observer:true//修改swiper自己或子元素时，自动初始化swiper
+        //     })
+        // },0)
         //活动推荐
-        $timeout(function () {
-            var swiper = new Swiper('.activity', {
-                effect: 'coverflow',  //切换效果
-                centeredSlides: true,  //显示居中状态
-                slidesPerView: 'auto',  //显示的多少个
-                loop : true,  //循环
-                coverflow: {
-                    rotate: 50,//slide做3d旋转时Y轴的旋转角度。默认50。
-                    stretch: 50,//每个slide之间的拉伸值，越大slide靠得越紧。 默认0。
-                    depth:0,  //slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
-                    modifier: 1,//depth和rotate和stretch的倍率，相当于depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
-                    slideShadows : false //开启slide阴影。默认 true。
-                }
-            });
-        },0)
+        // $timeout(function () {
+        //     var swiper = new Swiper('.activity', {
+        //         effect: 'coverflow',  //切换效果
+        //         centeredSlides: true,  //显示居中状态
+        //         slidesPerView: 'auto',  //显示的多少个
+        //         loop : true,  //循环
+        //         coverflow: {
+        //             rotate: 50,//slide做3d旋转时Y轴的旋转角度。默认50。
+        //             stretch: 50,//每个slide之间的拉伸值，越大slide靠得越紧。 默认0。
+        //             depth:0,  //slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
+        //             modifier: 1,//depth和rotate和stretch的倍率，相当于depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
+        //             slideShadows : false //开启slide阴影。默认 true。
+        //         }
+        //     });
+        // },0)
+        //列表
+            var swiper = new Swiper('.Repertoire_container', {
+                slidesPerView: 'auto',
+                spaceBetween:0,
+                 pagination: '.Repertoire-pagination',//分页容器
+                // prevButton:'.swiper-button-prev',
+                // nextButton:'.swiper-button-next',
+                observer:true,//修改swiper自己或子元素时，自动初始化swiper
+
+            })
+            $timeout(function () {
+                /**增加class */
+                // swiper.prevButton.addClass("swiper-button-disabled");
+                // /**移除class */
+                // swiper.nextButton.removeClass("swiper-button-disabled");
+                swiper.setWrapperTransition(0);
+                /**设置位移0 显示第一页 */
+                swiper.setWrapperTranslate(0);
+              //console.log(swiper.realIndex);
+              // $(".Repertoire-wrapper").css('transform','translate3d(0px, 0px, 0px)');
+           },500)
         //点击跳转
-        var _wrapper = document.getElementById("swiper-wrapper");
+       /* var _wrapper = document.getElementById("swiper-wrapper");
         _wrapper.addEventListener("click",function (e) {
             switch (e.target.id){
                 case "activity-item-1":
@@ -95,5 +118,5 @@ stareal
                     window.location.href='#/my/share'
                     break;
             }
-        },false)
+        },false)*/
     });

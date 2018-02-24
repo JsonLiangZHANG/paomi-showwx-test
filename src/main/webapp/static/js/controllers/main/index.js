7785e7bd7f69c17e7eee954561d3b94bd594e47f
pc <964115468@qq.com>
2018-02-24 17:28:53 +0800

    微信分享

--- src/main/webapp/static/js/controllers/main/index.js
+++ src/main/webapp/static/js/controllers/main/index.js
@@ -142,33 +142,61 @@ stareal
                         signature: data.signature,
                         jsApiList: [
                             'onMenuShareTimeline',
-                            'onMenuShareAppMessage'
-                            // 'onMenuShareQQ',
+                            'onMenuShareAppMessage',
+                            'onMenuShareQQ',
                             // 'onMenuShareWeibo',
-                            // 'onMenuShareQZone'
+                            'onMenuShareQZone'
                         ]
                     });
                     wx.ready(function(res){
                         //分享到朋友圈
                         wx.onMenuShareTimeline({
                             title: '独角秀', // 分享标题
-                            link: window.location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            desc: '独角秀--泡米文化传媒(上海)有限公司', // 分享描述
+                            link: location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                             imgUrl: 'http://image.mydeershow.com/2018-02-11.png', // 分享图标
                             success: function () {
                                 // 用户确认分享后执行的回调函数
+                                //alert('你好');
                             },
                             cancel: function () {
                                 // 用户取消分享后执行的回调函数
+                               // alert('你好....');
                             }
                         });
                         //分享给朋友
                         wx.onMenuShareAppMessage({
                             title: '独角秀', // 分享标题
-                            desc: '独角秀--泡米文化传媒(上海)有限公司 (企业)', // 分享描述
-                            link: window.location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            desc: '独角秀--泡米文化传媒(上海)有限公司', // 分享描述
+                            link: location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                             imgUrl: 'http://image.mydeershow.com/2018-02-11.png', // 分享图标
                             type: '', // 分享类型,music、video或link，不填默认为link
                             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                            success: function () {
+// 用户确认分享后执行的回调函数
+                                alert('你好....');
+                            },
+                            cancel: function () {
+// 用户取消分享后执行的回调函数
+                            }
+                        });
+                        wx.onMenuShareQQ({
+                            title: '独角秀', // 分享标题
+                            desc: '独角秀--泡米文化传媒(上海)有限公司', // 分享描述
+                            link: location.href.split('#')[0], // 分享链接
+                            imgUrl: 'http://image.mydeershow.com/2018-02-11.png', // 分享图标
+                            success: function () {
+// 用户确认分享后执行的回调函数
+                            },
+                            cancel: function () {
+// 用户取消分享后执行的回调函数
+                            }
+                        });
+                        wx.onMenuShareQZone({
+                            title: '独角秀', // 分享标题
+                            desc: '独角秀--泡米文化传媒(上海)有限公司', // 分享描述
+                            link: location.href.split('#')[0] ,// 分享链接
+                            imgUrl: 'ttp://image.mydeershow.com/2018-02-11.png', // 分享图标
                             success: function () {
 // 用户确认分享后执行的回调函数
                             },

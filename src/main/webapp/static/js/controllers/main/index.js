d83e7ca489749615de3d99ab5c5544cdcdd91fd7
pc <964115468@qq.com>
2018-02-24 16:43:13 +0800

    微信分享

--- src/main/webapp/static/js/controllers/main/index.js
+++ src/main/webapp/static/js/controllers/main/index.js
@@ -121,32 +121,38 @@ stareal
         },false)*/
 
        //微信分享http://192.168.1.4:9090/oauth/getSignature
-        $api.get("app/share/getSignature",{url: encodeURIComponent(window.location.href.split('#')[0])})
+        $api.get("app/share/getSignature",{url: window.location.href.split('#')[0]})
             .then(function (ret) {
+
                 if (ret) {
                     console.log(ret);
                    var data=ret.data;
                     console.log('-------------------------------');
-                    console.log(ret);
+                    console.log(data);
+                    console.log(data.appid);
+                    console.log(data.timestamp);
+                    console.log(data.nonceStr);
+                    console.log(data.signature);
+
                     wx.config({
-                        debug: false,
+                        debug: true,
                         appId: data.appid,
                         timestamp: data.timestamp,
                         nonceStr: data.nonceStr,
                         signature: data.signature,
                         jsApiList: [
                             'onMenuShareTimeline',
-                            'onMenuShareAppMessage',
-                            'onMenuShareQQ',
-                            'onMenuShareWeibo',
-                            'onMenuShareQZone'
+                            'onMenuShareAppMessage'
+                            // 'onMenuShareQQ',
+                            // 'onMenuShareWeibo',
+                            // 'onMenuShareQZone'
                         ]
                     });
                     wx.ready(function(res){
                         //分享到朋友圈
                         wx.onMenuShareTimeline({
                             title: '独角秀', // 分享标题
-                            link: 'http://m.mydeershow.com/?#/main/index', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            link: window.location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                             imgUrl: 'http://image.mydeershow.com/2018-02-11.png', // 分享图标
                             success: function () {
                                 // 用户确认分享后执行的回调函数
@@ -159,7 +165,7 @@ stareal
                         wx.onMenuShareAppMessage({
                             title: '独角秀', // 分享标题
                             desc: '独角秀--泡米文化传媒(上海)有限公司 (企业)', // 分享描述
-                            link: 'http://m.mydeershow.com/?#/main/index', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            link: window.location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                             imgUrl: 'http://image.mydeershow.com/2018-02-11.png', // 分享图标
                             type: '', // 分享类型,music、video或link，不填默认为link
                             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -172,6 +178,7 @@ stareal
                         });
                     });
                     wx.error(function(res){
+                        console.log(res);
                         alert("微信分享接口配置失败");
                     });
                 }

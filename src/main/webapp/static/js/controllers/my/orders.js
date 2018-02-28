2e2435507a1a841dd5fb3f1dd10e02cb7e75c1b5
oyyp <googleoyyp@163.com>
2018-02-28 19:57:50 +0800

    微信zhifu修复

--- src/main/webapp/static/js/controllers/my/orders.js
+++ src/main/webapp/static/js/controllers/my/orders.js
@@ -125,10 +125,10 @@ stareal
                                 }, function (res) {
                                     if (res.err_msg == "get_brand_wcpay_request:ok") {
                                         // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                                        $scope.showpay = false;
+                                        $(".mask_pay").fadeOut();
                                         alert("支付成功!");
                                        // $scope.showpay = false;
-                                        $state.go("my.orders", {});
+                                        $state.reload('my.orders');
                                     } else if (res.err_msg == "get_brand_wcpay_request:cancel") {
                                         alert("您已取消支付,订单将在15分钟后取消!");
                                         $state.go("my.orders", {});
